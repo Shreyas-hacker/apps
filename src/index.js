dc1c1e340506d753e851a9793b174dcf782ed177
Shreyas-hacker <shreyashegde1@gmail.com>
2021-06-24 01:12:22 +0800

    Added JSX file to add Heading function

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Heading from "./time";
 
-const date = new Date();
-const time = date.getHours();
-var style = {color: "red"};
-
-if(time >= 0 && time < 12)
-{
-  style.color = "red";
-} else if(time >= 12 && time < 6){
-  style.color = "green";
-} else{
-  style.color = "blue";
-}
 
 ReactDOM.render(
   <div>
-    <h1 className="heading" style= {style}>Hello World!</h1>
+    <Heading />
   </div>,
   document.getElementById("root")
 );
