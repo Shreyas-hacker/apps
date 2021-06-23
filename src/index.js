7e087f657fe93d6e3904691c0fee944f72bab96b
Shreyas-hacker <shreyashegde1@gmail.com>
2021-06-24 01:04:48 +0800

    Added time functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+const date = new Date();
+const time = date.getHours();
+var style = {color: "red"};
+
+if(time >= 0 && time < 12)
+{
+  style.color = "red";
+} else if(time >= 12 && time < 6){
+  style.color = "green";
+} else{
+  style.color = "blue";
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <h1 className="heading" style= {style}>Hello World!</h1>
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
