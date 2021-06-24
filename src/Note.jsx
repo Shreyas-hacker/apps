50454637df8a5ebae0e54fb64e4282e43c6191c8
Shreyas-hacker <shreyashegde1@gmail.com>
2021-06-24 19:01:20 +0800

    added functionalities to make keeper app

--- src/Note.jsx
+++ src/Note.jsx
@@ -1,2 +1,14 @@
 //5. Create a Note.jsx component to show a <div> element with a
 //<h1> for a title and a <p> for the content.
+import React from "react";
+
+function Note(){
+    return(
+        <div className="note">
+            <h1>This is note title</h1>
+            <p>This is note content</p>
+        </div>
+    );
+}
+
+export default Note;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 //1. Create a new React app.
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
