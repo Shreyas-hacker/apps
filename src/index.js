2bbf1b9063af071c6d756c06ee616eb1fcbb2e7f
Shreyas-hacker <shreyashegde1@gmail.com>
2021-06-25 01:49:49 +0800

    relocated jsx files to components folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //1. Create a new React app.
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 ReactDOM.render(
