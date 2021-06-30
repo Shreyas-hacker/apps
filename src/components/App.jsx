1f0447678ec39c03061bad44adde26ec9675f2c0
Shreyas-hacker <shreyashegde1@gmail.com>
2021-06-30 18:53:43 +0800

    added a area to make notes and delete notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,15 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
+
 
 function App(){
     return (
         <div>
             <Header />
-            {notes.map(note=><Note key={note.key} title={note.title} content={note.content}/>)}
+            <CreateArea />
+            <Note />
             <Footer />
         </div>
     );
